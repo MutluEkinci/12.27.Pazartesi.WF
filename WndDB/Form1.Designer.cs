6bf0af21171fca0fc8131528fd5a55f8c38732fd
203AKSAM_MUTLU <mutlu.e@hotmail.com>
2021-12-28 18:32:13 +0300

    new

--- WndDB/Form1.Designer.cs
+++ WndDB/Form1.Designer.cs
@@ -38,13 +38,23 @@ namespace WndDB
             this.lblSoyad = new System.Windows.Forms.Label();
             this.lblAd = new System.Windows.Forms.Label();
             this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBaglantısızSelect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBğlGridDoldur = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(58, 234);
+            this.btnSelect.Location = new System.Drawing.Point(15, 177);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(167, 45);
+            this.btnSelect.Size = new System.Drawing.Size(186, 45);
             this.btnSelect.TabIndex = 0;
             this.btnSelect.Text = "Bağlantılı Yöntem (Select)";
             this.btnSelect.UseVisualStyleBackColor = true;
@@ -54,36 +64,36 @@ namespace WndDB
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(58, 285);
+            this.listBox1.Location = new System.Drawing.Point(15, 228);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(442, 276);
+            this.listBox1.Size = new System.Drawing.Size(391, 228);
             this.listBox1.TabIndex = 1;
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(260, 56);
+            this.txtID.Location = new System.Drawing.Point(123, 31);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(160, 22);
+            this.txtID.Size = new System.Drawing.Size(214, 22);
             this.txtID.TabIndex = 2;
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(260, 84);
+            this.txtAd.Location = new System.Drawing.Point(123, 59);
             this.txtAd.Name = "txtAd";
-            this.txtAd.Size = new System.Drawing.Size(160, 22);
+            this.txtAd.Size = new System.Drawing.Size(214, 22);
             this.txtAd.TabIndex = 2;
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(260, 112);
+            this.txtSoyad.Location = new System.Drawing.Point(123, 87);
             this.txtSoyad.Name = "txtSoyad";
-            this.txtSoyad.Size = new System.Drawing.Size(160, 22);
+            this.txtSoyad.Size = new System.Drawing.Size(214, 22);
             this.txtSoyad.TabIndex = 2;
             // 
             // lblID
             // 
             this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(149, 61);
+            this.lblID.Location = new System.Drawing.Point(12, 36);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(71, 17);
             this.lblID.TabIndex = 3;
@@ -92,7 +102,7 @@ namespace WndDB
             // lblSoyad
             // 
             this.lblSoyad.AutoSize = true;
-            this.lblSoyad.Location = new System.Drawing.Point(149, 117);
+            this.lblSoyad.Location = new System.Drawing.Point(12, 92);
             this.lblSoyad.Name = "lblSoyad";
             this.lblSoyad.Size = new System.Drawing.Size(98, 17);
             this.lblSoyad.TabIndex = 3;
@@ -101,7 +111,7 @@ namespace WndDB
             // lblAd
             // 
             this.lblAd.AutoSize = true;
-            this.lblAd.Location = new System.Drawing.Point(149, 89);
+            this.lblAd.Location = new System.Drawing.Point(12, 64);
             this.lblAd.Name = "lblAd";
             this.lblAd.Size = new System.Drawing.Size(75, 17);
             this.lblAd.TabIndex = 3;
@@ -109,19 +119,103 @@ namespace WndDB
             // 
             // btnKaydet
             // 
-            this.btnKaydet.Location = new System.Drawing.Point(330, 140);
+            this.btnKaydet.Location = new System.Drawing.Point(123, 115);
             this.btnKaydet.Name = "btnKaydet";
-            this.btnKaydet.Size = new System.Drawing.Size(90, 30);
+            this.btnKaydet.Size = new System.Drawing.Size(75, 30);
             this.btnKaydet.TabIndex = 4;
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.UseVisualStyleBackColor = true;
             this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(356, 30);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(50, 23);
+            this.btnAra.TabIndex = 5;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(3, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(79, 30);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(88, 3);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(48, 30);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(204, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(138, 35);
+            this.panel1.TabIndex = 8;
+            // 
+            // btnBaglantısızSelect
+            // 
+            this.btnBaglantısızSelect.Location = new System.Drawing.Point(207, 177);
+            this.btnBaglantısızSelect.Name = "btnBaglantısızSelect";
+            this.btnBaglantısızSelect.Size = new System.Drawing.Size(199, 45);
+            this.btnBaglantısızSelect.TabIndex = 9;
+            this.btnBaglantısızSelect.Text = "Bağlantısız Yöntem (Select)";
+            this.btnBaglantısızSelect.UseVisualStyleBackColor = true;
+            this.btnBaglantısızSelect.Click += new System.EventHandler(this.btnBaglantısızSelect_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(471, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(483, 228);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 482);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 95);
+            this.textBox1.TabIndex = 11;
+            // 
+            // btnBğlGridDoldur
+            // 
+            this.btnBğlGridDoldur.Location = new System.Drawing.Point(471, 177);
+            this.btnBğlGridDoldur.Name = "btnBğlGridDoldur";
+            this.btnBğlGridDoldur.Size = new System.Drawing.Size(203, 45);
+            this.btnBğlGridDoldur.TabIndex = 12;
+            this.btnBğlGridDoldur.Text = "Bağlantılı Yöntem Grid Doldur";
+            this.btnBğlGridDoldur.UseVisualStyleBackColor = true;
+            this.btnBğlGridDoldur.Click += new System.EventHandler(this.btnBğlGridDoldur_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1409, 588);
+            this.ClientSize = new System.Drawing.Size(1409, 608);
+            this.Controls.Add(this.btnBğlGridDoldur);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBaglantısızSelect);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAra);
             this.Controls.Add(this.btnKaydet);
             this.Controls.Add(this.lblAd);
             this.Controls.Add(this.lblSoyad);
@@ -133,6 +227,8 @@ namespace WndDB
             this.Controls.Add(this.btnSelect);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +245,14 @@ namespace WndDB
         private System.Windows.Forms.Label lblSoyad;
         private System.Windows.Forms.Label lblAd;
         private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBaglantısızSelect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBğlGridDoldur;
     }
 }
 
--- WndDB/Form1.cs
+++ WndDB/Form1.cs
@@ -33,7 +33,7 @@ namespace WndDB
             string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
             SqlConnection conn = new SqlConnection(strConn);
             conn.Open();
-            SqlCommand cmd = new SqlCommand("select*from musteri", conn);
+            SqlCommand cmd = new SqlCommand("select*from musteri order by musıd", conn);
 
             SqlDataReader dr = cmd.ExecuteReader();
 
@@ -49,8 +49,6 @@ namespace WndDB
                 strSatir = "";
             }
             //listBox1.Items.Add(dr["musID"]);
-
-            
 
             conn.Close();
         }
@@ -73,6 +71,173 @@ namespace WndDB
             cmd.Parameters.AddWithValue("@soyad", txtSoyad.Text);
 
             cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+
+            string strSQLID = "select*from musteri where Musıd=@musid";
+
+            SqlCommand cmd = new SqlCommand(strSQLID, conn);
+
+            cmd.Parameters.AddWithValue("@musid", int.Parse(txtID.Text));
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            dr.Read();
+
+            if (dr.HasRows)
+            {
+                txtAd.Text = dr[1].ToString();
+                txtSoyad.Text = dr[2].ToString();
+
+                panel1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Aradığınız kayıt bulunamadı.");
+                txtAd.Clear();
+                txtSoyad.Clear();
+            }
+
+            //string strSatir = "";
+            //while (dr.Read())
+            //{
+            //    for (int i = 0; i < dr.FieldCount; i++)
+            //    {
+            //        strSatir += dr.GetValue(i) + " ";
+            //    }
+            //    listBox1.Items.Add(strSatir);
+            //    strSatir = "";
+            //}
+            //conn.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+
+            string strSQLUpdate = "";
+
+            strSQLUpdate = "update musteri set musad=@ad,mussoyad=@soyad where musıd=@musid";
+            SqlCommand cmd = new SqlCommand(strSQLUpdate, conn);
+
+            cmd.Parameters.AddWithValue("@musid", int.Parse(txtID.Text));
+            cmd.Parameters.AddWithValue("@ad", txtAd.Text);
+            cmd.Parameters.AddWithValue("@soyad", txtSoyad.Text);
+
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+
+            DialogResult dr = MessageBox.Show("Kaydı silmek istiyor musunuz?", "UYARI", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                conn.Open();
+
+                string strSQLDelete = "Delete from musteri where musıd=@id";
+
+                SqlCommand cmd = new SqlCommand(strSQLDelete, conn);
+
+                cmd.Parameters.AddWithValue("@id", int.Parse(txtID.Text));
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        private void btnBaglantısızSelect_Click(object sender, EventArgs e)
+        {
+            
+
+            string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
+
+            //da.SelectCommand = new SqlCommand("Select*from musteri");
+            //da.SelectCommand.Connection.ConnectionString=strConn;
+
+            SqlDataAdapter da = new SqlDataAdapter("select*from musteri order by musıd",strConn);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            DataRow satir = dt.NewRow();
+
+            satir[0] = 155;
+            satir[1] = "cevdettt";
+            satir[2] = "korkmazzz";
+            
+            //dt.Rows.Add(satir);
+
+            dt.Rows[1][1] = "....";
+
+            SqlCommandBuilder scb = new SqlCommandBuilder(da);
+
+            textBox1.Text = scb.GetUpdateCommand().CommandText;
+
+            dataGridView1.DataSource = dt;
+            // MessageBox.Show(dt.Rows.Count.ToString());
+
+            string strSatir = "";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    strSatir += dt.Rows[i][j].ToString() + " ";
+                }
+                listBox1.Items.Add(strSatir + dt.Rows[i].RowState);
+
+                strSatir = "";
+            }
+            da.Update(dt);
+        }
+
+        private void btnBğlGridDoldur_Click(object sender, EventArgs e)
+        {
+            string strConn = @"Data Source=DESKTOP-TUMHS1A\MS_SQL_2019;Initial Catalog=Calısma;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select*from musteri order by musıd", conn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dataGridView1.ColumnCount = dr.FieldCount;
+            object[] veriler = new object[dr.FieldCount];
+           
+            //COLUMN
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                dataGridView1.Columns[i].Name = dr.GetName(i);
+            }
+            
+            //DATA
+            while (dr.Read())
+            {
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    veriler[i] = dr[i];
+                }
+                
+                dataGridView1.Rows.Add(veriler);
+            
+            }
+            
             conn.Close();
         }
     }
